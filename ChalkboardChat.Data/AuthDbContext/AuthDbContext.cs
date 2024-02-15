e2e352376b2662a237ba03fdc7c1c1454834f5f6
emeliehensfelt <emelie046@gmail.com>
2024-02-15 11:34:06 +0100

    databas

---
+++ ChalkboardChat.Data/AuthDbContext/AuthDbContext.cs
@@ -0,0 +1,12 @@
+﻿namespace ChalkboardChat.Data.AuthDbContext
+{
+    public class AuthDbContext : IdentityDbContext
+    {
+        public AuthDbContext(DbContextOptions<AuthDbContext> options) : base(options)
+        {
+
+        }
+
+
+    }
+}
--- ChalkboardChat.Data/Model/ChalkboardModel.cs
+++ ChalkboardChat.Data/Model/ChalkboardModel.cs
@@ -1,12 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace ChalkboardChat.Data.Model
 {
-    internal class ChalkboardModel
+    public class ChalkboardModel
     {
+        [Key]
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public string Message { get; set; }
+        public string Username { get; set; }
     }
 }
