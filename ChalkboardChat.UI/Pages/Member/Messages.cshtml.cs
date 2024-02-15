45304bcd32848a8aa0f26486d8c208662888f307
Broscan <Calle992015@gmail.com>
2024-02-15 16:13:51 +0100

    added messages typ

--- ChalkboardChat.UI/Pages/Member/Messages.cshtml.cs
+++ ChalkboardChat.UI/Pages/Member/Messages.cshtml.cs
@@ -9,15 +9,22 @@ namespace ChalkboardChat.UI.Pages.Member
         private readonly SignInManager<IdentityUser> _signInManager;
         public string? Username { get; set; }
         public string? Password { get; set; }
+        public string? Message { get; set; }
 
         public MessagesModel(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
         {
             _userManager = userManager;
             _signInManager = signInManager;
+
+
         }
         public void OnGet()
         {
             _signInManager.UserManager.GetUserAsync(HttpContext.User);
+        }
+
+        public void OnPost()
+        {
 
         }
     }
